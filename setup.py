83376d948e07d9f744caa6b18c3e97f6e58fc216
Jon Moen Drange <jondrange@gmail.com>
2022-05-23 09:52:27 +0200

    Release v0.8

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Topic :: Office/Business :: Financial",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
